f965e10e786ba8c8c5fab821fb783e4d0de18e6f
xdx1354 <stanislaw1354@gmail.com>
2022-11-08 12:28:47 +0100

    Initial commit

---
+++ src/main/java/Product.java
@@ -0,0 +1,10 @@
+public class Product {
+    String name;
+    Integer quantity;
+    Float price;
+    Product(String name, Integer quantity, Float price){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class main {
+
+
+
+
+
+    public static void main(String []Args){
+        String filename = Args[0];
+        File fileOpen = new File(filename);
+    }
+}
